72f5659169d1e8c958709cb7f1e788016823d1d1
Deepak Kumar Roul <kumardeepakroul17@gmail.com>
2024-07-29 20:34:07 +0530

    first commit

---
+++ app/src/main/java/com/internshala/flash/data/InternetItemWithQuantity.kt
@@ -0,0 +1,6 @@
+package com.internshala.flash.data
+
+data class InternetItemWithQuantity(
+    val item:InternetItem,
+    val quantity: Int
+)
---
+++ app/src/main/java/com/internshala/flash/ui/FlashUiState.kt
@@ -0,0 +1,6 @@
+package com.internshala.flash.ui
+
+data class FlashUiState(
+    val clickStatus:String="Hello View Models",
+    val selectedCategory: Int = 0
+)
